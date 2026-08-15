db5e4de61b46bcaa96c5b14d884c4a0d5464b8c6
Daniel <dani.ricci93@gmail.com>
2026-08-15 10:26:15 -0300

    Create login endpoint.

--- casa-do-hamburguer/back-end/index.ts
+++ casa-do-hamburguer/back-end/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
-import { Connect } from "./src/db.js"
+import { Connect } from "./src/db.js";
+import { prisma } from "./src/db.js";
 
 const app = express();
+app.use(express.json());
 Connect();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findFirst({
+    where: { email: email, password: password },
+  });
+  res.json(user);
 });
 
 app.listen(3000, () => {
